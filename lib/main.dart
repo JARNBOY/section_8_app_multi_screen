c155298ef4e8e3eafee8379aa9041ff5e2366a36
Papon S <paponsup@scg.com>
2023-11-21 07:33:54 +0700

    [Flutter-153] Widgets vs Screens (Reviewed by Papon S.)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:section_8_app_multi_screen/categories.dart';
+import 'package:section_8_app_multi_screen/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
